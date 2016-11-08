62ec00eed140fb9eedb4aa9c8adaadd2b589a088
aidarochin <aidalizethr@hotmail.com>
2016-11-08 12:28:57 -0700

    Solucionando conflictos con login y registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,7 @@
 		<div class="container">
 			<ol class="breadcrumb breadcrumb1 animated wow slideInLeft" data-wow-delay=".5s">
 				<li><a href="{{url('/inicio')}}"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Inicio</a></li>
-				<li class="active">Register Page</li>
+				<li class="active">Registro</li>
 			</ol>
 		</div>
 	</div>
@@ -14,19 +14,84 @@
 <!-- register -->
 	<div class="register">
 		<div class="container">
-			<h3 class="animated wow zoomIn" data-wow-delay=".5s">Registrese Aqui</h3>
+			<h3 class="animated wow zoomIn" data-wow-delay=".5s">Registrese Aquí</h3>
 			{{-- <p class="est animated wow zoomIn" data-wow-delay=".5s">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
 				deserunt mollit anim id est laborum.</p> --}}
 			<div class="login-form-grids">
 				{{-- <h5 class="animated wow slideInUp" data-wow-delay=".5s">profile information</h5> --}}
 			    <form class="animated wow slideInUp" data-wow-delay=".5s" role="form" method="POST" action="{{ url('/register') }}">
                     {{ csrf_field() }}
-					<input type="text" name="username" placeholder="Usuario" required=" " >
-					<input type="text" name="full_name" placeholder="Nombre Completo" required=" " >
-					<br>
-					<input type="email" name="email" placeholder="Correo Electronico" required=" " >
-					<input type="password" name="password" placeholder="Contraseña" required=" " >
-					<input type="password" name="password_confirmation" placeholder="Confirmación Contraseña" required=" " >
+
+						<div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
+                            <!-- <label for="username" class="col-md-4 control-label">Usuario</label> -->
+
+                            <!-- <div class="col-md-12"> -->
+                                <input id="username" type="text" class="form-control" name="username" placeholder="Usuario" value="{{ old('username') }}" required autofocus>
+
+                                @if ($errors->has('username'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('username') }}</strong>
+                                    </span>
+                                @endif
+                            <!-- </div> -->
+                        </div>
+
+						<div class="form-group{{ $errors->has('full_name') ? ' has-error' : '' }}">
+                            <!-- <label for="full_name" class="col-md-4 control-label">Usuario</label> -->
+
+                            <!-- <div class="col-md-12"> -->
+                                <input id="full_name" type="text" class="form-control" name="full_name" placeholder="Nombre Completo" value="{{ old('full_name') }}" required autofocus>
+
+                                @if ($errors->has('full_name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('full_name') }}</strong>
+                                    </span>
+                                @endif
+                            <!-- </div> -->
+                        </div>
+                        
+						<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <!-- <label for="email" class="col-md-4 control-label">E-Mail</label> -->
+
+                            <!-- <div class="col-md-6"> -->
+                                <input id="email" type="email" class="form-control" name="email" placeholder="Correo Electronico" value="{{ old('email') }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            <!-- </div> -->
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <!-- <label for="password" class="col-md-4 control-label">Contraseña</label> -->
+
+                            <!-- <div class="col-md-6"> -->
+                                <input id="password" type="password" class="form-control" name="password" placeholder="Contraseña" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            <!-- </div> -->
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                            <!-- <label for="password-confirm" class="col-md-4 control-label">Confirmar Contraseña</label> -->
+
+                            <!-- <div class="col-md-6"> -->
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Confirmar Contraseña" required>
+
+                                @if ($errors->has('password_confirmation'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                    </span>
+                                @endif
+                            <!-- </div> -->
+                        </div>
+
 					<div class="register-check-box">
 						<div class="check">
 							<label class="checkbox"><input type="checkbox" name="checkbox"><i> </i>Acepto los términos y condiciones</label>
