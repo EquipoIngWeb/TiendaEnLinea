bce3a2014c55a07b9a88ede243ff4cab461b7258
ramon321 <joseramonperez46@gmail.com>
2016-11-16 23:57:02 -1200

    Owl carousel

---
+++ resources/views/components/article.blade.php
@@ -0,0 +1,37 @@
+
+<div class="col-md-12 new-collections-grid">
+	<div class="new-collections-grid1 animated wow slideInUp" data-wow-delay=".5s">
+		<div class="new-collections-grid1-image">
+			<a href="single.html" class="product-image"><img src="{{asset('administrador/images/si.jpg')}}" alt=" " class="img-responsive" /></a>
+			<div class="new-collections-grid1-image-pos">
+				<a href="">Ver más</a>
+			</div>
+			<div class="new-collections-grid1-right">
+				<div class="rating">
+					<div class="rating-left">
+						<img src="images/2.png" alt="" class="img-responsive" />
+					</div>
+					<div class="rating-left">
+						<img src="images/2.png" alt="" class="img-responsive" />
+					</div>
+					<div class="rating-left">
+						<img src="images/2.png" alt="" class="img-responsive" />
+					</div>
+					<div class="rating-left">
+						<img src="images/1.png" alt="" class="img-responsive" />
+					</div>
+					<div class="rating-left">
+						<img src="images/1.png" alt="" class="img-responsive" />
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+		</div>
+		<h4><a href="single.html">Blusa rayada</a></h4>
+		<p>Bonita y comoda.</p>
+		<div class="new-collections-grid1-left simpleCart_shelfItem">
+			<p><i>$325</i> <span class="item_price">$250</span><a class="item_add" href="#">al carrito </a></p>
+		</div>
+	</div>
+	
+</div>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -99,6 +99,16 @@
 				<p class="est animated wow zoomIn" data-wow-delay=".5s">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
 					deserunt mollit anim id est laborum.</p>
 					<div class="new-collections-grids">
+
+						<div id="owl-demo" class="owl-carousel owl-theme">
+							@include('components.article')
+							@include('components.article')
+							@include('components.article')
+							@include('components.article')
+						</div>
+						
+								
+						{{--
 						<div class="col-md-3 new-collections-grid">
 							<div class="new-collections-grid1 animated wow slideInUp" data-wow-delay=".5s">
 								<div class="new-collections-grid1-image">
@@ -133,40 +143,7 @@
 									<p><i>$325</i> <span class="item_price">$250</span><a class="item_add" href="#">al carrito </a></p>
 								</div>
 							</div>
-							<div class="new-collections-grid1 animated wow slideInUp" data-wow-delay=".5s">
-								<div class="new-collections-grid1-image">
-									<a href="single.html" class="product-image"><img src="{{asset('administrador/images/tenis.jpg')}}" alt=" " class="img-responsive" /></a>
-									<div class="new-collections-grid1-image-pos">
-										<a href="single.html">Ver más</a>
-									</div>
-									<div class="new-collections-grid1-right">
-										<div class="rating">
-										<!-- estrellas -->
-											<div class="rating-left">
-												<img src="images/2.png" alt=" " class="img-responsive" />
-											</div>
-											<div class="rating-left">
-												<img src="images/2.png" alt=" " class="img-responsive" />
-											</div>
-											<div class="rating-left">
-												<img src="images/1.png" alt=" " class="img-responsive" />
-											</div>
-											<div class="rating-left">
-												<img src="images/1.png" alt=" " class="img-responsive" />
-											</div>
-											<div class="rating-left">
-												<img src="images/1.png" alt=" " class="img-responsive" />
-											</div>
-											<div class="clearfix"> </div>
-										</div>
-									</div>
-								</div>
-								<h4><a href="single.html">Tennis</a></h4>
-								<p>Para ir a toda prisa.</p>
-								<div class="new-collections-grid1-left simpleCart_shelfItem">
-									<p><i>$280</i> <span class="item_price">$150</span><a class="item_add" href="#">al carrito </a></p>
-								</div>
-							</div>
+							
 						</div>
 						<div class="col-md-6 new-collections-grid">
 							<div class="new-collections-grid1 new-collections-grid1-image-width animated wow slideInUp" data-wow-delay=".5s">
@@ -347,6 +324,7 @@
 								</div>
 							</div>
 						</div>
+						--}}
 						<div class="clearfix"> </div>
 					</div>
 				</div>
