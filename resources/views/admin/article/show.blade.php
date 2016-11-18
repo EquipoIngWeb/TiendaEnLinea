8c7a0202fa46ac72c0d67e9d6624ee5bac7eb00c
Veronica Valenzuela <verito_hp@live.com.mx>
2016-11-18 14:16:44 -0700

    Arreglando style de Productos

--- resources/views/admin/article/show.blade.php
+++ resources/views/admin/article/show.blade.php
@@ -52,7 +52,7 @@ Articulo {{$product->name}}
 										<div class="modal-header">
 											<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 											<h4 class="modal-title" id="myModalLabel">
-											Cambiar nombre de imagen <span class="category">{{$image['name']}}</span>
+											Cambiar nombre a <span class="category">{{$image['name']}}</span>
 											</h4>
 										</div>
 										<form action="{{ url('admin/imagen/change') }}" action="POST">
