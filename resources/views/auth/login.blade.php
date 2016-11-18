4ee60bd27ef44e21d33334314cdfe3139a2ec753
ramon321 <joseramonperez46@gmail.com>
2016-11-18 01:41:22 -1200

    Login Actualizado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,88 +1,54 @@
 @extends('layouts.app')
-
 @section('content')
-<!-- breadcrumbs -->
-	<div class="breadcrumbs">
-		<div class="container">
-			<ol class="breadcrumb breadcrumb1 animated wow slideInLeft" data-wow-delay=".2s">
-				<li><a href="{{url ('/')}}"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Inicio</a></li>
-				<li class="active">Inicio de Sesión</li>
-			</ol>
-		</div>
-	</div>
-<!-- //breadcrumbs -->
+
 <!-- login -->
-	<div class="login">
-		<div class="container">
-			<h3 class="animated wow zoomIn" data-wow-delay=".2s">
+	<div class="container row">
+		<div class="col s12 m6 offset-m3">
+			<h1 class="center">
 				Iniciar Sesión
-			</h3>
-			{{-- <p class="est animated wow zoomIn" data-wow-delay=".2s">
-				Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-				deserunt mollit anim id est laborum.
-			</p>
-			 --}}
-			<div class="login-form-grids animated wow slideInUp" data-wow-delay=".2s">
-				<form  role="form" method="POST" action="{{ url('/login') }}">
-					{{ csrf_field() }}
-
-					<div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
-                            <!-- <label for="username" class="col-md-4 control-label">Usuario</label> -->
-
-                            <!-- <div class="col-md-6"> -->
-                                <input id="username" type="text" class="form-control" name="username" placeholder="Usuario" value="{{ old('username') }}" required autofocus>
-
-                                @if ($errors->has('username'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('username') }}</strong>
-                                    </span>
-                                @endif
-                            <!-- </div> -->
-                        </div>
+			</h1>
 
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <!-- <label for="password" class="col-md-4 control-label">Contraseña</label>
+			<form  role="form" method="POST" action="{{ url('/login') }}" class="col s12">
+				{{ csrf_field() }}
+				<div class="input-field col s12">
+					<input id="username" type="text" class="form-control" name="username" value="{{ old('username') }}" required autofocus>
+					<label for="user">Usuario</label>
+				</div>
+				<div class="input-field col s12">
+					<input id="password" type="password" class="form-control" name="password" required>
+					<label for="password">Contraseña</label>
+				</div>
 
-                            <div class="col-md-6"> -->
-                                <input id="password" type="password" class="form-control" name="password" placeholder="Contraseña" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            <!-- </div> -->
-                        </div>
-
-
-					<!-- <input type="text" name="username" placeholder="Usuario" value="{{ old('username') }}" required autofocus> -->
-					<!-- <input type="password" name="password" placeholder="Contraseña" required > -->
-					<div class="register-check-box animated wow slideInUp" data-wow-delay=".2s">
-						<div class="check">
-							<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Recordarme</label>
-						</div>
-					</div>
-					<div class="forgot">
-						<a href="{{ url('/password/reset') }}">
-							Olvido su contraseña?
-						</a>
-					</div>
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
+				@if (count($errors) > 0)
+					<div class="row">
+						<div class="col s12 red center white-text">
 							@foreach ($errors->all() as $error)
 								{{ $error }} <br>
 							@endforeach
 						</div>
-					@endif
-
-					<input type="submit" value="Login">
-				</form>
-			</div>
-			<h4 class="animated wow slideInUp" data-wow-delay=".2s">Para nuevos clientes</h4>
-			<div class="register-home animated wow slideInUp" data-wow-delay=".2s">
-				<a href="{{ url('register') }}">Registrese Aquí</a>
-			</div>
+					</div>
+				@endif
+				<div class="row">
+					<div class="col offset-s1">
+						<input type="checkbox" id="remember_me" />
+						<label for="remember_me">Recordarme</label>
+					</div>
+				</div>
+				<div class="center">
+					<input type="submit" value="Login" class="btn blue">
+					<a href="{{ url('/password/reset') }}" class="btn red">
+						Olvido su contraseña?
+					</a>
+				</div>
+				<br>
+				<div class="col s12 center">
+					<b>¿ Nuevo por aqui ?</b>
+					<div>
+						<a href="{{ url('register') }}">Registrese</a>
+					</div>
+				</div>
+			</form>
 		</div>
 	</div>
 <!-- //login -->
